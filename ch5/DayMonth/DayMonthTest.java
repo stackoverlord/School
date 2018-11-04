45995fc15bf4072143ec1727f98456ea298d8046
Dean <34590133+stackoverlord@users.noreply.github.com>
2018-11-04 10:38:54 -0700

    formated daymonth

--- ch5/DayMonth/DayMonthTest.java
+++ ch5/DayMonth/DayMonthTest.java
@@ -1,9 +1,6 @@
-package cds.chapters.ch5.DayMonth;
 
-public class DayMonthTest 
-{
-	public static void main(String[] args)
-	{
+public class DayMonthTest {
+	public static void main(String[] args) {
 		DayMonth month = new DayMonth();
 		month.getMonth();
 	}
