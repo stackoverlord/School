039fe6206b78b83cbee22428d57fe7faf2ee1af7
Dean <34590133+stackoverlord@users.noreply.github.com>
2018-11-27 16:38:31 -0700

    updated quad formula

--- ch5/QuadEqua/QuadEqua.java
+++ ch5/QuadEqua/QuadEqua.java
@@ -1,70 +1,21 @@
-package cds.chapters.ch5.QuadEqua;
-import javax.swing.JOptionPane;
+import java.util.*;
 
-public class QuadEqua 
-{
-	public QuadEqua()
-	{
-		String str1 = JOptionPane.showInputDialog("A = ____");
-		a = Double.parseDouble(str1);
-		if (a <= 0)
-		{
-			str1 = JOptionPane.showInputDialog("Please Reenter A (No Negatives)");
-			a = Double.parseDouble(str1);
-		}
-		String str2 = JOptionPane.showInputDialog("B = ____");
-		b = Double.parseDouble(str2);
-		if (b <= 0)
-		{
-			str2 = JOptionPane.showInputDialog("Please Reenter B (No Negatives)");
-			b = Double.parseDouble(str2);
-		}
-		String str3 = JOptionPane.showInputDialog("C = ____");
-		c = Double.parseDouble(str3);
-		if (c <= 0)
-		{
-			str2 = JOptionPane.showInputDialog("Please Reenter C (No Negatives)");
-			a = Double.parseDouble(str3);
-		}
-	}
-	public void getSol1()
-	{
-		sol1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
-	}
-	public void getSol2()
-	{
-		sol2 = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
-	}
-	public void hasSol()
-	{
-		dis = Math.pow(b, 2) - (4 * a * c);
-		if (dis < 0)
-		{
-			disc = false;
-		}
-		else 
-		{
-			disc = true;
-		}
-	}
-	public void ret()
-	{
-		if (disc == false)
-		{
-			System.out.println("There are no real Solutions.");
-		}
-		else
-		{
-			System.out.println("The three numbers are: " + a + " " + b + " " + c);
-			System.out.println("Equation: " + a + "x^2 + " + b + "x + " + c);
-			System.out.println("Roots: " + sol1 + " and " + sol2);
-		}
-	}
-	private double a;
-	private double b;
-	private double c;
-	private double dis;
-	private boolean disc;
-	private double sol1;
-	private double sol2;
+public class QuadForm {
+
+
+public static void main(String[]args) {
+  System.out.println("insert value for a:");
+  Scanner scan1 = new Scanner(System.in);
+   double a = scan1.nextDouble();
+    System.out.println("insert value for b:");
+  Scanner scan2 = new Scanner(System.in);
+    double b = scan2.nextDouble();
+    System.out.println("insert value for C:");
+  Scanner scan3 = new Scanner(System.in);
+   double c = scan3.nextDouble();
+
+    double answer =((Math.sqrt(Math.pow(b,2)-(4*a*c))-b)/2);
+      double final2 =(-b + Math.sqrt(Math.pow(b,2)-(4*a*c)))/2;
+  System.out.println("The x values are:" + answer + final2);
+}
 }
--- ch5/QuadEqua/QuadEquaTest.java
+++ ch5/QuadEqua/QuadEquaTest.java
@@ -1,13 +1,25 @@
-package cds.chapters.ch5.QuadEqua;
+import java.util.*;
 
-public class QuadEquaTest 
-{
-	public static void main(String[] args)
-	{
-		QuadEqua quad = new QuadEqua();
-		quad.getSol1();
-		quad.getSol2();
-		quad.hasSol();
-		quad.ret();
-	}
+public class QuadFormTest {
+
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Insert value for a: ");
+        double a = Double.parseDouble(s.nextLine());
+        System.out.println("Insert value for b: ");
+        double b = Double.parseDouble(s.nextLine());
+        System.out.println("Insert value for c: ");
+        double c = Double.parseDouble(s.nextLine());
+        s.close();
+
+        double answer1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+        double answer2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+
+        if (Double.isNaN(answer1) || Double.isNaN(answer2))
+        {
+            System.out.println("Answer contains imaginary numbers");
+        } else System.out.println("The values are: " + answer1 + ", " + answer2);
+    }
 }
+
