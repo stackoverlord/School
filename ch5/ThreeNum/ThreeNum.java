89f91c8ff3fe591a5d756c8f27a470487c8acfab
Dean <34590133+stackoverlord@users.noreply.github.com>
2018-12-02 17:55:03 -0700

    fixed threenum
    fixed threenum

--- ch5/ThreeNum/ThreeNum.java
+++ ch5/ThreeNum/ThreeNum.java
@@ -1,109 +1,41 @@
-package cds.chapters.ch5.ThreeNum;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+//dean pesavento
+//per5
+
+
+import java.util.Arrays;
 
 public class ThreeNum 
 {
-	public ThreeNum() throws FileNotFoundException
-	{
-		FileReader reader = new FileReader("src/cds/chapters/ch5/Numbers.dat");
-		Scanner file = new Scanner(reader);
-		num1 = file.nextFloat();
-		num2 = file.nextFloat();
-		num3 = file.nextFloat();
-		num4 = file.nextFloat();
-		num5 = file.nextFloat();
-		num6 = file.nextFloat();
-		num7 = file.nextFloat();
-		num8 = file.nextFloat();
-		num9 = file.nextFloat();
+
+	private double array[] = new double[3]; 
+	
+	public ThreeNum() {
+		
+	} // end ThreeNum
+
+	public void setThreeNum(double pNum1, double pNum2, double pNum3) {
+	
+		array[0] = pNum1;
+		array[1] = pNum2;
+		array[2] = pNum3;
+		
+		Arrays.sort(array);
 	}
-	public void debug()
+		
+	public double getLargest()
 	{
-		System.out.println(num1);
-		System.out.println(num2);
-		System.out.println(num3);
-		System.out.println(num4);
-		System.out.println(num5);
-		System.out.println(num6);
-		System.out.println(num7);
-		System.out.println(num8);
-		System.out.println(num9);
-	}
-	public void first3()
+		return array[2];
+	} // end getLargest
+	
+	public double getMiddle()
 	{
-		System.out.println("The First Three Inputs are: " + num1 + "   " + num2 + "   " + num3);
-		max1 = Math.max(num1, Math.max(num2, num3));
-		min1 = Math.min(num1, Math.min(num2, num3));
-		if ((max1 == num1 || max1 == num2) && (min1 == num1 || min1 == num2))
-		{
-			mid1 = num3;
-		}
-		else if ((max1 == num2 || max1 == num3) && (min1 == num2 || min1 == num3))
-		{
-			mid1 = num1;
-		}
-		else
-		{
-			mid1 = num2;
-		}
-		System.out.println("Sorted Inputs:  " + min1 + "   " + mid1 + "   " + max1);
-	}
-	public void second3()
+		return array[1];
+	} // end getMiddle
+	
+	public double getSmallest()
 	{
-		System.out.println("The Second Three Inputs are: " + num4 + "   " + num5 + "   " + num6);
-		max2 = Math.max(num4, Math.max(num5, num6));
-		min2 = Math.min(num4, Math.min(num5, num6));
-		if ((max2 == num4 || max2 == num5) && (min2 == num4 || min2 == num5))
-		{
-			mid2 = num6;
-		}
-		else if ((max2 == num5 || max2 == num6) && (min2 == num5 || min2 == num6))
-		{
-			mid2 = num4;
-		}
-		else
-		{
-			mid2 = num5;
-		}
-		System.out.println("Sorted Inputs:  " + min2 + "   " + mid2 + "   " + max2);
-	}
-	public void third3()
-	{
-		System.out.println("The Third Three Inputs are: " + num7 + "   " + num8 + "   " + num9);
-		max3 = Math.max(num7, Math.max(num8, num9));
-		min3 = Math.min(num7, Math.min(num8, num9));
-		if ((max3 == num7 || max3 == num8) && (min3 == num7 || min3 == num8))
-		{
-			mid3 = num9;
-		}
-		else if ((max3 == num8 || max3 == num9) && (min3 == num8 || min3 == num9))
-		{
-			mid3 = num7;
-		}
-		else
-		{
-			mid3 = num8;
-		}
-		System.out.println("Sorted Inputs:  " + min3 + "   " + mid3 + "   " + max3);
-	}
-	private float num1;
-	private float num2;
-	private float num3;
-	private float num4;
-	private float num5;
-	private float num6;
-	private float num7;
-	private float num8;
-	private float num9;
-	private float max1;
-	private float min1;
-	private float mid1;
-	private float max2;
-	private float min2;
-	private float mid2;
-	private float max3;
-	private float min3;
-	private float mid3;
+		return array[0];
+	} // end getSmallest
+	
+	
 }
--- ch5/ThreeNum/ThreeNumTest.java
+++ ch5/ThreeNum/ThreeNumTest.java
@@ -1,16 +1,47 @@
-package cds.chapters.ch5.ThreeNum;
+//dean pesavento
+//per 5
+
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class ThreeNumTest 
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		ThreeNum num = new ThreeNum();
-		//num.debug();
-		num.first3();
-		System.out.println("");
-		num.second3();
-		System.out.println("");
-		num.third3();
+
+		double num1;
+		double num2;
+		double num3;
+		
+		FileReader reader = new FileReader("src/Numbers.dat");
+		Scanner file = new Scanner(reader);
+		num1 = file.nextDouble();
+		num2 = file.nextDouble();
+		num3 = file.nextDouble();
+		
+		num.setThreeNum(num1, num2, num3);
+		
+		System.out.printf("First set of inputs: %6.1f %6.1f %6.1f\n", num1, num2, num3);
+		System.out.printf("Sorted inputs: %6.1f %6.1f %6.1f\n", num.getSmallest(), num.getMiddle(), num.getLargest());
+		
+		num1 = file.nextDouble();
+		num2 = file.nextDouble();
+		num3 = file.nextDouble();
+		
+		num.setThreeNum(num1, num2, num3);
+		
+		System.out.printf("Second set of inputs: %6.1f %6.1f %6.1f\n", num1, num2, num3);
+		System.out.printf("Sorted inputs: %6.1f %6.1f %6.1f\n", num.getSmallest(), num.getMiddle(), num.getLargest());
+		
+		num1 = file.nextDouble();
+		num2 = file.nextDouble();
+		num3 = file.nextDouble();
+		file.close();
+		num.setThreeNum(num1, num2, num3);
+	
+		System.out.printf("Third set of inputs: %6.1f %6.1f %6.1f\n", num1, num2, num3);
+		System.out.printf("Sorted inputs: %6.1f %6.1f %6.1f\n", num.getSmallest(), num.getMiddle(), num.getLargest());
 	}
 }
